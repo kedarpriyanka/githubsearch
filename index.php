306a9d9274afcd5db7faec2d76164d09e004cb9a
Priyanka Kedar <kedar.priyanka96@gmail.com>
2023-03-09 16:29:35 -0600

    added 3 new files added db config

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once __DIR__ . "/config.php";
+require_once __DIR__ . "/db-config.php";
+
+global $dbHost, $dbPort, $dbUser, $dbPassword, $dbName;
+
+$connection = mysqli_connect($dbHost, $dbUser, $dbPassword, $dbName, $dbPort);
+
+
+
