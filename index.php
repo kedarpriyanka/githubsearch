ba6a8dc681ad9966a42edbdc7c2f1d2b5f2e0f7b
Priyanka Kedar <kedar.priyanka96@gmail.com>
2023-03-09 18:59:03 -0600

    created search elements in page

--- index.php
+++ index.php
@@ -7,7 +7,32 @@ global $dbHost, $dbPort, $dbUser, $dbPassword, $dbName;
 
 
 
-$connection = mysqli_connect($dbHost, $dbUser, $dbPassword, $dbName, $dbPort);
+//$connection = mysqli_connect($dbHost, $dbUser, $dbPassword, $dbName, $dbPort);
+
+$html = <<<HTML
+<html>
+<head>
+
+</head>
+<body>
+<h1>Search Tool</h1>
+<br>
+<label for = "text">search text</label> 
+<input type = "text" id="text" value = "roses">   &nbsp; &nbsp; &nbsp; &nbsp;
+<label for = "element">choose an element</label>
+<select name = "element" id = "element" > 
+<option value = "repository">repository</option>
+<option value = "user">user</option>
+</select>
+&nbsp;&nbsp;&nbsp;&nbsp;
+<input type = "button" value="search">
+</body>
+</html>
+HTML;
+
+echo $html;
+
+
 
 
 
